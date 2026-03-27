018847a882d54893fb35b80ec7abf4f75ce1cfa1
Brady <bradywenzbauer@mail.weber.edu>
2026-03-27 11:21:23 -0600

    3/27/2026 update 4

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,2 +1,24 @@
 //TODO - Your ES6 JavaScript code (if any) goes here
 import "bootstrap"
+
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+const closeBtn = document.querySelector(".lightbox-close");
+
+document.querySelectorAll(".gallery-item img").forEach(img => {
+  img.addEventListener("click", () => {
+    lightbox.style.display = "block";
+    lightboxImg.src = img.src;
+  });
+});
+
+closeBtn.addEventListener("click", () => {
+  lightbox.style.display = "none";
+});
+
+// Click outside image to close
+lightbox.addEventListener("click", (e) => {
+  if (e.target === lightbox) {
+    lightbox.style.display = "none";
+  }
+});
